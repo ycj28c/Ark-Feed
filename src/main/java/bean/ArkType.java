8c47c16b0f1f7a3954fcc88635d4e39a71a247b6
ycj28c <ycj28c@gmail.com>
2021-02-20 16:32:32 -0800

    no message

--- src/main/java/bean/ArkType.java
+++ src/main/java/bean/ArkType.java
@@ -8,11 +8,11 @@ public enum ArkType {
     /**
      * ark type, and its csv link
      */
-    ARKK("ARKK_OLD_JSON", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"),
-    ARKQ("ARKQ_OLD_JSON", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.csv"),
-    ARKW("ARKW_OLD_JSON", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS.csv"),
-    ARKG("ARKG_OLD_JSON", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_GENOMIC_REVOLUTION_MULTISECTOR_ETF_ARKG_HOLDINGS.csv"),
-    ARKF("ARKF_OLD_JSON", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.csv"),
+    ARKK("arkk_old_result.json", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"),
+    ARKQ("arkq_old_result.json", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.csv"),
+    ARKW("arkw_old_result.json", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS.csv"),
+    ARKG("arkg_old_result.json", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_GENOMIC_REVOLUTION_MULTISECTOR_ETF_ARKG_HOLDINGS.csv"),
+    ARKF("arkf_old_result.json", "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.csv"),
     ;
 
     private final String fileName;
