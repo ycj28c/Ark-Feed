10fe74b0b1aaac5074bd869da91d911948b6878b
ycj28c <ycj28c@gmail.com>
2021-03-10 11:17:36 -0800

    commit

--- src/main/java/utils/ArkFileUtils.java
+++ src/main/java/utils/ArkFileUtils.java
@@ -99,6 +99,7 @@ public class ArkFileUtils {
             URL url12 = new URL(baseurl);
             URLConnection urlConn = url12.openConnection();
             urlConn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36");
+            InputStreamReader inStream = new InputStreamReader(urlConn.getInputStream());
             BufferedReader buff = new BufferedReader(inStream);
 
             String line = buff.readLine();
